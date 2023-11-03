b026eaab5b02939dce84128c2ee2f8e0f5e71ff0
kraken013 <sofiane.flici14@gmail.com>
2023-11-03 11:28:20 +0100

    first commit
    - installation
    - components
    - navbar

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Navbar from "./components/Navbar.svelte";
+</script>
+
+
+	<Navbar />
+	<section style="display:flex; flex-direction:columns; width: 100%;">
+		<div style="background-color: greenyellow; height:95vh; width:10%;">
+
+		</div>
+		<div style="background-color: red; height:95vh; width:90%;">
+
+		</div>
+	</section>
---
+++ svelte-app/src/components/Navbar.svelte
@@ -0,0 +1,19 @@
+<nav class="navbar">
+    <div class="navbar-left">
+        <button class="menu-btn">☰</button> 
+        <div class="logo">
+            <img src="" alt="logo" /> 
+        </div>
+    </div>
+
+    <div class="search-bar">
+        <input type="text" placeholder="Rechercher">
+        <button class="search-btn">🔍</button> 
+    </div>
+
+    <div class="navbar-right">
+        <button class="btn-create">📽️</button>
+        <button class="btn-notifications">🔔</button>
+        <button>👤</button> 
+    </div>
+</nav>
